7e94bc12065a1b484968fde50e97a9fe26efb48a
Boolean-Operator <marktgraybill@gmail.com>
2018-07-24 19:04:01 -0400

    Sect 10 Syntac Sugar Convert Hangman from proto inheritance to Class constructor

--- hangman/app.js
+++ hangman/app.js
@@ -18,37 +18,3 @@ window.addEventListener('keypress', function (e) {
   usedLettersEl.textContent = `Used Letters: ${game.guessedLetters}`;
   
 });
-
-
-
-
-// const wordElement = document.querySelector('#puzzle-word')
-// const remainingTurns = document.querySelector('#puzzle-turns')
-// const submitWordButton = document.querySelector('#word-input')
-
-// const guessElement = document.querySelector('#letter-guess')
-// const guessButton = document.querySelector('#make-guess')
-
-
-// submitWordButton.addEventListener('click', (e) => {
-  //   const game = new Hangman(wordElement.value, remainingTurns.value);
-  //   console.log(game);
-  //   console.log(game.getPuzzle());
-  // })
-  
-  
-  
-  // guessButton.addEventListener('click', (e) => {
-    //   let guess = guessElement.value
-    //   console.log(guess);
-    //   Hangman.makeGuess(guess)
-    // })
-    
-    
-    // window.addEventListener('keypress', function (e) {
-      //   const guess = e.key;
-      //   game1.makeGuess(guess)
-      //   console.log(game1.getPuzzle());
-      //   console.log(` `);
-
-      // })
