803f364ce07a1c20e1a3a0041ab607b8ec40c831
Boolean-Operator <marktgraybill@gmail.com>
2018-07-24 19:00:12 -0400

    Sect 10.  Syntactical Sugar - refactor from Proto Inheritance to Class constructor

--- oop/person.js
+++ oop/person.js
@@ -1,3 +1,33 @@
+class PersonClass {
+  constructor(firstName, lastName, age, likes = []) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.likes = likes
+  }
+  getBio() {
+    let bio = `${this.firstName} is ${this.age}.`
+
+    this.likes.forEach((like) => {
+      bio += ` ${this.firstName} likes ${like}.`
+    })
+    return bio
+  }
+  setName(fullName) {
+    const name = fullName.split(' ')
+    this.firstName = name[0]
+    this.lastName = name[1]
+
+  }
+}
+
+const myPerson = new PersonClass('Mark', 'Graybill', 52, ['Building wooden boats', 'Biking', 'Snow skiing'])
+console.log(myPerson.getBio());
+myPerson.setName('Logan Graybill');
+console.log(myPerson.getBio());
+
+
+
 const Person = function (fName, lName, age, likes= []) {
   this.firstName = fName
   this.lastName = lName
