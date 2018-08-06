7e58aa8e9fe3b9566194799b993ef0ea417199d6
Boolean-Operator <marktgraybill@gmail.com>
2018-08-06 15:24:58 -0400

    Improve card flip functionality

--- card-flip/cardFlip.js
+++ card-flip/cardFlip.js
@@ -1,20 +1,16 @@
-    
-word = "puzzle problem";
+const wordEl = document.querySelector('#puzzle')
+
+word = "Longer puzzle problem";
 puzzleWord = word.toUpperCase().split('');
 word = word.toUpperCase().split('')
 word = word.reverse()
-console.log(word);
-console.log(puzzleWord);
-
-
-const wordEl = document.querySelector('#puzzle')
 
 window.addEventListener('keypress', (e) => {
   let guess = e.key;
   guess = guess.toUpperCase()
   for (let index = 0; index < puzzleWord.length; index++) {
     if (puzzleWord[index] === guess) {
-      cards[index].classList.toggle('is-flipped')
+      cards[index].classList.add('is-flipped')
     }
   }
 })
@@ -23,16 +19,14 @@ word.forEach((letter) => {
   return wordEl.insertAdjacentHTML('afterbegin',`<div class="scene scene--card"><div class="card"><span class="card__face card__face--front"></span><span class="card__face card__face--back">${letter}</span>< /div></div>`);
 })
 
-console.log(wordEl);
 var cards = document.querySelectorAll('.card');
-
-
-
-for (let index = 0; index < cards.length; index++) {
-  cards[index].addEventListener('click', function () {
-    cards[index].classList.toggle('is-flipped');
-  });
+for (let index = 0; index < puzzleWord.length; index++) {
+  if (puzzleWord[index] === ' ') {
+    cards[index].classList.toggle('is-flipped')
+  }
 }
 
+// integrate flip feature into hangman for WoF game 
+// add unique and badGuess filter 
 
 
